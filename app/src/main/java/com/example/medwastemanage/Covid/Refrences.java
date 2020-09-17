21d5532974ce2b76ce82067ada3361286305a7c4
ANURAG SINGH <f>
2020-09-17 23:43:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/medwastemanage/Covid/Refrences.java
@@ -0,0 +1,16 @@
+package com.example.medwastemanage.Covid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.medwastemanage.R;
+
+public class Refrences extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refrences);
+    }
+}
